ff5df3b6a17a63963d63cb7ec9c3e56027e7f2f0
mailtojkn <mailtojkn@gmail.com>
2026-07-18 21:52:56 -0700

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit=0;
+        for(int i=0;i<prices.Length;i++)
+        {
+            minPrice = Math.Min(minPrice, prices[i]);
+            maxProfit =  Math.Max(maxProfit, prices[i]-minPrice);
+        }
+        
+        return maxProfit;
+    }
+}
